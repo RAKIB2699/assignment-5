7e75e4931a172e8207be0c80b9798662db5ac6b1
RAKIB2699 <rakibhasan01645172699@gmail.com>
2025-03-02 16:42:41 +0600

    notification section implementation

--- scricpts/implement.js
+++ scricpts/implement.js
@@ -21,6 +21,40 @@ document.getElementById("themebtn").addEventListener('click', function () {
 
 })
 
-document.getElementById("blogbtn").addEventListener('click', function () { 
+document.getElementById("blogbtn").addEventListener('click', function () {
     window.location.href = "main.html"
 })
+
+const btns = document.querySelectorAll(".completebtn");
+    let value1 = parseInt(document.getElementById("navValue").innerText);
+    let value2 = parseInt(document.getElementById("taskValue").innerText);
+for(const btn of btns){
+    btn.addEventListener('click',function(e){
+       btn.setAttribute("disabled","true");
+       value1+=1;
+       value2-=1;
+       document.getElementById("navValue").innerText = value1;
+       document.getElementById("taskValue").innerText = value2;
+       alert("Board updated successfully");
+       let now = new Date().toLocaleTimeString();
+       const container = document.getElementById("container");
+       const text = btn.parentNode.parentNode;
+       const findText = text.children[1].innerText;
+       const newTitle = document.createElement('p');
+       newTitle.innerText =`you have complete the task ${findText} at ${now}`
+       container.append(newTitle);
+       newTitle.style.backgroundColor = "#F4F7FF";
+       newTitle.style.padding ="10px";
+       newTitle.style.margin = "10px";
+       newTitle.style.borderRadius ="5px";
+
+       var disabledButtons = document.querySelectorAll('btn:disabled').length;
+       
+            if (disabledButtons === 6) {
+                alert('All 6 buttons clicked successfully!');
+            }
+    })
+}
+
+
+document.getElementById("")
